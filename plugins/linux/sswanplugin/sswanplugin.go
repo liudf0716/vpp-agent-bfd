edf3a2d1d63b9dac52689fb2fa12599cda53a310
Dengfeng Liu <liudf0716@gmail.com>
2022-03-05 18:01:52 +0800

    Update sswanplugin.go

--- plugins/linux/sswanplugin/sswanplugin.go
+++ plugins/linux/sswanplugin/sswanplugin.go
@@ -28,6 +28,12 @@ import (
 )
 
 const (
+	// by default, at most 10 go routines will split the configured namespaces
+	// to execute the Retrieve operation in parallel.
+	defaultGoRoutinesCnt = 10
+)
+
+const (
         IpsecConfigFile      = "/usr/local/etc/ipsec.conf"
         StrongswanConfigFile = "/usr/local/etc/strongswan.conf"
         IpsecSecretsFile     = "/usr/local/etc/ipsec.secrets"
@@ -48,10 +54,50 @@ type Deps struct {
   KVScheduler kvs.KVScheduler
 }
 
+// Config holds the sswanplugin configuration.
+type Config struct {
+	Disabled      bool `json:"disabled"`
+	GoRoutinesCnt int  `json:"go-routines-count"`
+}
+
 func (p *SswanPlugin) Init() error {
-  return nil
+    // parse configuration file
+	config, err := p.retrieveConfig()
+	if err != nil {
+		return err
+	}
+	p.Log.Debugf("Linux strongswan plugin config: %+v", config)
+	if config.Disabled {
+		p.disabled = true
+		p.Log.Infof("Disabling Linux Strongswan plugin")
+		return nil
+	}
+    
+    sswanDescriptor := descriptor.NewSswanDescriptor(p.KVScheduler, p.Log, config.GoRoutinesCnt)
+    err = p.Deps.KVScheduler.RegisterKVDescriptor(sswanDescriptor)
+    if err != nil {
+            p.Log.Infof("sswan plugin register descriptor failed: %+v", err)
+            return err
+    }
+    return nil
 }
 
 func (p *SswanPlugin) Close() error {
   return nil
+}
+
+func (p *SswanPlugin) retrieveConfig() (*Config, error) {
+        config := &Config{
+                // default configuration
+                GoRoutinesCnt: defaultGoRoutinesCnt,
+        }
+        found, err := p.Cfg.LoadValue(config)
+        if !found {
+                p.Log.Debug("Linux SswanPlugin config not found")
+                return config, nil
+        }
+        if err != nil {
+                return nil, err
+        }
+        return config, err
 }
